e81e097830affce778866a2ee1357c31d0a99341
Sourish Bhattacharyya <sourish215@gmail.com>
2022-06-12 09:17:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import AddBook from './components/AddBook';
 import BookList from "./components/BookList";
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  uri: '/graphql',
   cache: new InMemoryCache()
 })
 
